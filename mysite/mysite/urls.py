17ac928495c96590f74ad780e84adbe30f4343fd
liuyongkang <liuyongkanglinux@gmail.com>
2013-12-09 14:42:50 +0800

    动态内容

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from mysite.views import hello
+from mysite.views import hello, current_time
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,5 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     ('^hello/$', hello),
+    ('^time/$', current_time),
+    ('^another-time-page/$', current_time),
 )
 
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,12 @@
 
 from django.http import HttpResponse
+import datetime
 
 def hello(request) :
     return HttpResponse("hello world")
+
+def current_time(request) :
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
 
