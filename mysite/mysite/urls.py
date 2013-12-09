24155eeeb42c9ac5dd069085d818e5355a939794
liuyongkang <liuyongkanglinux@gmail.com>
2013-12-09 14:49:25 +0800

    动态url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,15 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from mysite.views import hello, current_time
+from mysite.views import hello, current_time, hours_ahead
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    ('^hello/$', hello),
-    ('^time/$', current_time),
-    ('^another-time-page/$', current_time),
+    (r'^hello/$', hello),
+    (r'^time/$', current_time),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
 )
 
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,5 +1,5 @@
 
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 import datetime
 
 def hello(request) :
@@ -8,5 +8,14 @@ def hello(request) :
 def current_time(request) :
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset) :
+    try :
+        offset = int(offset)
+    except ValueError :
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours = offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
     return HttpResponse(html)
 
